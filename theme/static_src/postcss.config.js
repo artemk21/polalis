3f70aaeb161a03dc4d69bae7b6769e3c26eedf1f
ArtemK21 <ArtemKucherenko210@gmail.com>
2019-03-09 00:00:00 -0500

    feat: update postcss.config

---
+++ theme/static_src/postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    "postcss-simple-vars": {},
+    "postcss-nested": {}
+  },
+}
+
+# touched on 2025-05-27T15:28:53.858480Z
+# touched on 2025-07-09T21:53:54.143503Z
