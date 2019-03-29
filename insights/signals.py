138e66b9077e7fbe27e8e5960cb3f100efcbe57d
ArtemK21 <ArtemKucherenko210@gmail.com>
2019-03-29 00:00:00 -0400

    chore: update .gitignore

---
+++ insights/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         UserProfile.objects.create(user=instance)
+
+# touched on 2025-05-27T15:29:10.581536Z
+# touched on 2025-07-09T21:54:11.531051Z
