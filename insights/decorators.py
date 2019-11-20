a0aa7a08ff9e71f15d5e0df73ce2d84e0053a7e2
ArtemK21 <ArtemKucherenko210@gmail.com>
2019-11-20 00:00:00 -0500

    chore: update helpers

--- insights/decorators.py
+++ insights/decorators.py
@@ -23,3 +23,4 @@ def role_required(required_role):
 # touched on 2025-07-09T21:55:57.335549Z
 # touched on 2025-07-09T21:56:12.465644Z
 # touched on 2025-07-09T21:56:16.818009Z
+# touched on 2025-07-09T21:57:34.706811Z
